da258c9f7ba74d830517753a26be903f6ab45db8
JamiePan <jamiexf.pan@gmail.com>
2022-03-06 17:27:08 +1100

    fix the code and add build ignore

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -46,7 +46,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
             return (
               <div
                 className={styles.item}
-                key={index.toString()}
+                // key={index.toString()}
                 onClick={onClick}
                 onKeyDown={() => {}}
                 role="menuitem"

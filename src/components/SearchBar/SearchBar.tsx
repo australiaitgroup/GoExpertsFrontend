5e219b6322b46e79a0eb5a7ff25e239d4d3763d8
BingTianEng <92073136+BingTianEng@users.noreply.github.com>
2022-03-08 22:00:45 +1100

    Update SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -46,7 +46,6 @@ const SearchBar: React.FC<SearchBarProps> = ({
             return (
               <div
                 className={styles.item}
-                key={index.toString()}
                 onClick={onClick}
                 onKeyDown={() => {}}
                 role="menuitem"
